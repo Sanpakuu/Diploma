40d057034b0e6a35e7811efcee7a4c1d8b38767b
Sanpakuu <andreyfom.1994@gmail.com>
2024-04-16 00:42:46 +0300

    sign modal visual

--- src/javascript/firebase-config.js
+++ src/javascript/firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDiL7cJ54cNtuDD676elAz0iSRMOaVth-U",
-  authDomain: "diplom-93856.firebaseapp.com",
-  projectId: "diplom-93856",
-  storageBucket: "diplom-93856.appspot.com",
-  messagingSenderId: "788892321907",
-  appId: "1:788892321907:web:e7597c191b5f3578efc206",
-  measurementId: "G-FX34DDGDHN"
+    apiKey: "AIzaSyDiL7cJ54cNtuDD676elAz0iSRMOaVth-U",
+    authDomain: "diplom-93856.firebaseapp.com",
+    projectId: "diplom-93856",
+    storageBucket: "diplom-93856.appspot.com",
+    messagingSenderId: "788892321907",
+    appId: "1:788892321907:web:e7597c191b5f3578efc206",
+    measurementId: "G-FX34DDGDHN",
 };
 
 // Initialize Firebase
@@ -15,14 +15,18 @@ const auth = firebase.auth();
 const database = firebase.database();
 
 function register() {
-    const login = document.getElementById('login').value;
-    const email = document.getElementById('email2').value;
-    const password = document.getElementById('password2').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+    const login = document.getElementById("login").value;
+    const email = document.getElementById("email2").value;
+    const password = document.getElementById("password2").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
 
-    if (validate_email(email) == false || validate_pass(password) == false || validate_field(login) == false) {
-       alert("Неверные данные");
-       return;
+    if (
+        validate_email(email) == false ||
+        validate_pass(password) == false ||
+        validate_field(login) == false
+    ) {
+        alert("Неверные данные");
+        return;
     }
 
     if (password != confirmPassword) {
@@ -30,68 +34,65 @@ function register() {
         return;
     }
 
-    auth.createUserWithEmailAndPassword(email, password)
-    .then(function() {
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(function () {
+            var user = auth.currentUser;
+            var database_ref = database.ref();
 
-        var user = auth.currentUser;
-        var database_ref = database.ref()
+            var user_data = {
+                login: login,
+                email: email,
+                pass: password,
+                last_login: Date.now(),
+            };
 
-        var user_data = {
-            login : login,
-            email : email,
-            pass : password,
-            last_login : Date.now()
-        }
+            database_ref.child("users/" + user.uid).set(user_data);
 
-        database_ref.child('users/' + user.uid).set(user_data)
+            alert("Пользователь создан");
+        })
+        .catch(function (error) {
+            var error_code = error.code;
+            var error_message = error.message;
 
-        alert("Пользователь создан")
-    })
-    .catch(function(error){
-        var error_code = error.code;
-        var error_message = error.message;
-
-        alert(error_message)
-    })
+            alert(error_message);
+        });
 }
 
 async function login() {
-    const email = document.getElementById('email1').value;
-    const password = document.getElementById('password1').value;
-    console.log(email)
-    if (validate_field(email) == false || validate_pass(password) == false ) {
+    const email = document.getElementById("email1").value;
+    const password = document.getElementById("password1").value;
+    console.log(email);
+    if (validate_field(email) == false || validate_pass(password) == false) {
         alert("Неверные данные");
         return;
-     }
-     
-     await auth.signInWithEmailAndPassword(email, password)
-     .then( function() {
-        
-        var user = auth.currentUser;
-        var database_ref = database.ref()
+    }
 
-        var user_data = {
-            last_login : Date.now() 
-        }
+    await auth
+        .signInWithEmailAndPassword(email, password)
+        .then(function () {
+            var user = auth.currentUser;
+            var database_ref = database.ref();
 
-        database_ref.child('users/' + user.uid).update(user_data)
+            var user_data = {
+                last_login: Date.now(),
+            };
 
-        alert("Ты вошел")
+            database_ref.child("users/" + user.uid).update(user_data);
 
-        // Сохраняем информацию о входе пользователя в localStorage
-        localStorage.setItem('isLoggedIn', 'true');
-        
-        // Перенаправляем пользователя на страницу "Личный кабинет"
-        window.location.href = "/cabinet";
-     })
-     .catch(function(error){
-        var error_message = error.message;
+            alert("Ты вошел");
 
-        alert(error_message)
-    })
+            // Сохраняем информацию о входе пользователя в localStorage
+            localStorage.setItem("isLoggedIn", "true");
 
+            // Перенаправляем пользователя на страницу "Личный кабинет"
+            window.location.href = "/cabinet";
+        })
+        .catch(function (error) {
+            var error_message = error.message;
 
-    
+            alert(error_message);
+        });
 }
 
 function validate_email(email1) {
@@ -103,12 +104,55 @@ function validate_email(email1) {
 
 function validate_pass(password) {
     if (password.length < 6) {
-        return false
+        return false;
     } else return true;
 }
 
 function validate_field(field) {
-    if(field == null || field.trim() === "") {
+    if (field == null || field.trim() === "") {
         return false;
     } else return true;
 }
+
+// Функция отправки почты (здесь может быть ваша логика отправки данных)
+function sendEmail() {
+    const email = document.getElementById("emailInput").value;
+    const modal = document.getElementById("myModal")
+    alert("Email отправлен на адрес: " + email);
+    modal.style.display = "none"; // Закрыть модальное окно после смены пароля
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("send-password").addEventListener("click", function () {
+        document.getElementById("myModal").style.display = "flex";
+    });
+
+    // Получаем модальное окно
+    var modal = document.getElementById("myModal");
+
+    // Получаем элемент <span>, который закрывает модальное окно
+    var span = document.getElementsByClassName("close")[0];
+
+    // При клике на <span> (x), закрыть модальное окно
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    // При клике вне модального окна, закрыть его
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    // Добавьте обработчик для формы смены пароля
+    document
+        .getElementById("passwordChangeForm")
+        .addEventListener("submit", function (e) {
+            e.preventDefault();
+            var oldPassword = document.getElementById("oldPassword").value;
+            var newPassword = document.getElementById("newPassword").value;
+            // Здесь код для смены пароля, используя oldPassword и newPassword
+            // Например, вызов функции смены пароля в Firebase или вашей собственной системе аутентификации
+        });
+});
